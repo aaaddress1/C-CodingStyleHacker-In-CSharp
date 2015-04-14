6fceb58a72c555015bc0763ff473c97c60cbc6f3
aaaddress1 <aaaddress1@gmail.com>
2015-04-14 20:31:41 +0800

    no message

---
+++ CodingStyleHacker/Form1.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace CodingStyleHacker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.metroButton1.Location = new System.Drawing.Point(20, 383);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(475, 23);
+            this.metroButton1.TabIndex = 6;
+            this.metroButton1.Text = "Source Code 混淆";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(20, 30);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.richTextBox1);
+            this.splitContainer1.Panel1.Controls.Add(this.metroLabel2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.richTextBox2);
+            this.splitContainer1.Panel2.Controls.Add(this.metroLabel1);
+            this.splitContainer1.Size = new System.Drawing.Size(475, 353);
+            this.splitContainer1.SplitterDistance = 226;
+            this.splitContainer1.TabIndex = 7;
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.metroLabel1.Location = new System.Drawing.Point(0, 0);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(243, 23);
+            this.metroLabel1.TabIndex = 0;
+            this.metroLabel1.Text = "混淆後Source Code";
+            this.metroLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.metroLabel2.Location = new System.Drawing.Point(0, 0);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(224, 23);
+            this.metroLabel2.TabIndex = 1;
+            this.metroLabel2.Text = "原始版本Source Code";
+            this.metroLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(0, 23);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(224, 328);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "#include <iostream>\nusing namespace std;\n\nint main()\n{\n    int i,sum=0;\n    for (" +
+    "i=0;i<101;i++)\n    {\n        sum+=i;\n    }\n    cout << sum << \"\\n\";\n    system(\"" +
+    "pause\");\n    return 0;\n}";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox2.Location = new System.Drawing.Point(0, 23);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(243, 328);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 426);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.metroButton1);
+            this.DisplayHeader = false;
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(20, 30, 20, 20);
+            this.ShadowType = MetroFramework.Forms.MetroFormShadowType.DropShadow;
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+    }
+}
+
---
+++ CodingStyleHacker/Form1.cs
@@ -0,0 +1,124 @@
+﻿using MetroFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace CodingStyleHacker
+{
+    public partial class Form1 : MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<string> RndVariableList = new List<string>();
+        string GetRandName()
+        {
+            string NewRandomVarName = "";
+            do
+            {
+                NewRandomVarName = "v_" + new Random().Next(5, 1000).ToString();
+            } while (RndVariableList.Contains(NewRandomVarName));
+            return (NewRandomVarName);
+        }
+
+        string ForToWhileMethod(string SourceCode)
+        {
+            while (SourceCode.Contains("for"))
+            {
+                int LBrack_count = 0,Semicolon_count = 0;
+                int ForPos = SourceCode.IndexOf("for");
+                int LBrackPos = SourceCode.IndexOf("(", ForPos);
+                int p = LBrackPos;
+                string[] ForContent = new string[3] {"","",""};
+
+                for (; ; p++)
+                {
+                    if (SourceCode[p] == ';')
+                    {
+                        Semicolon_count++;
+                        continue;
+                    }
+                    else if (SourceCode[p] == '(')
+                    {
+                        LBrack_count++;
+                        if (LBrack_count == 1) continue;
+                    }
+                    else if (SourceCode[p] == ')')
+                    {
+                        LBrack_count--;
+                        if (LBrack_count == 0) break;
+                    }
+                    ForContent[Semicolon_count] += SourceCode[p];
+                }
+
+                if (ForContent[2] != "") SourceCode = SourceCode.Insert(SourceCode.IndexOf("}", p), ForContent[2] + ";\n");
+                SourceCode = SourceCode.Remove(LBrackPos + 1, p - LBrackPos - 1);
+                SourceCode = SourceCode.Insert(LBrackPos + 1, ForContent[1]);
+                SourceCode = SourceCode.Remove(ForPos, 3);
+                SourceCode = SourceCode.Insert(ForPos, "while");
+                SourceCode = SourceCode.Insert(ForPos, ForContent[0] + ";\n");
+           }
+            return (SourceCode);
+        }
+        private string RemoveStr(string Source,string[] Patten)
+        {
+            foreach (string item in Patten) Source = Source.Replace(item, null);
+            return (Source);
+        }
+
+        private string GetManyBlankStr(int count)
+        {
+            string Tempstr = "";
+            for (; count > 0; count--) Tempstr += "\x09";
+            return (Tempstr);
+        }
+
+        private string BeautyMyCode(string Source)
+        {
+            Source = RemoveStr(Source, new string[] { "\n", "\x09", "  " });
+            Source = new Regex("#include <(.*?)>").Replace(Source, "$0\n");
+            Source = new Regex(";").Replace(Source, "$0\n");
+            Source = new Regex("{").Replace(Source, "\n$0\n");
+            Source = new Regex("}").Replace(Source, "$0\n");
+            int DeepCount = 0;
+            for (int i = 0; i < Source.Length; i++)
+            {
+                if (Source[i] == '}') DeepCount--;
+                else if (Source[i] == '{') DeepCount++;
+                else if (Source[i]  == '\n')
+                {
+                    int NextNewLinePos = Source.IndexOf("\n", i + 1);
+                    if (NextNewLinePos != -1)
+                    {
+                        Console.WriteLine(Source.Substring(i, NextNewLinePos - i));
+                        if (Source.Substring(i, NextNewLinePos - i).Contains("}"))
+                        {
+                            Source = Source.Insert(i + 1, GetManyBlankStr(DeepCount -1));
+                        }
+                        else
+                        {
+                            Source = Source.Insert(i + 1, GetManyBlankStr(DeepCount));
+                        }
+                        
+                    }
+                }
+            }
+            return (Source);
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            string SourceCode = richTextBox1.Text;
+            SourceCode = ForToWhileMethod(SourceCode);
+            richTextBox2.Text = BeautyMyCode(SourceCode);
+        }
+
+    }
+}
